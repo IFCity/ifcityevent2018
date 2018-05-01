49296ea21be6d9193a3a03bea4cb01fdc8683688
Zoreslav Goral <zoreslavgoral@Zoreslavs-MacBook-Pro.local>
2018-05-01 17:49:49 +0300

    add favicon

--- src/ssr/server.js
+++ src/ssr/server.js
@@ -84,6 +84,13 @@ export function pageRender(config) {
                     <title>${config.getPageTitle(route, data, match)}</title>
                     <link href="/main.css" rel="stylesheet">
                     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&amp;subset=cyrillic,cyrillic-ext,latin-ext" rel="stylesheet">
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+                    <link rel="manifest" href="/site.webmanifest">
+                    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+                    <meta name="msapplication-TileColor" content="#2d89ef">
+                    <meta name="theme-color" content="#ffffff">
                 </head>
                 <body>
                     <div id="root">${html}</div>
